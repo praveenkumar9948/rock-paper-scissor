38a64f30464dd59e510e1d1dbf0623e4f1a26ae7
pk <badanapurampk@iitbhilai.ac.in>
2023-03-14 19:20:52 +0530

    added buttons

--- game.py
+++ game.py
@@ -15,6 +15,7 @@ comp_label = Label(root,image=rock_i,bg="black")
 comp_label.grid(row=1,column=4)
 
 
+
 uscore = Label(root,text=0,font=100,bg="black",fg="white")
 uscore.grid(row=1,column=1)
 cscore = Label(root,text=0,font=100,bg="black",fg="white")
@@ -28,4 +29,11 @@ comp_indica.grid(row=0,column=3)
 msg = Label(root,font=50,bg="black",fg="white")
 msg.grid(row=3,column=2)
 // added labels and score.
+
+rock = Button(root,width=20,height=2,text="ROCK",bg="black",fg="white").grid(row=2,column=1)
+paper = Button(root,width=20,height=2,text="PAPER",bg="black",fg="white").grid(row=2,column=2)
+sciss = Button(root,width=20,height=2,text="SCISSORS",bg="black",fg="white").grid(row=2,column=3)
+
+
+
 root.mainloop()

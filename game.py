434ec9338636490ee24c8487d3472dbb357ece5e
pk <badanapurampk@iitbhilai.ac.in>
2023-03-14 19:20:52 +0530

    added buttons

--- game.py
+++ game.py
@@ -14,4 +14,10 @@ user_label.grid(row=1,column=0)
 comp_label = Label(root,image=rock_i,bg="black")
 comp_label.grid(row=1,column=4)
 
+
+rock = Button(root,width=20,height=2,text="ROCK",bg="black",fg="white").grid(row=2,column=1)
+paper = Button(root,width=20,height=2,text="PAPER",bg="black",fg="white").grid(row=2,column=2)
+sciss = Button(root,width=20,height=2,text="SCISSORS",bg="black",fg="white").grid(row=2,column=3)
+
+
 root.mainloop()

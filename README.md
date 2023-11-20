4e8d51b9786138d46f944c87cafecea77c2b7b52
praveenkumar9948 <badanapurampraveenkumar5@gmail.com>
2023-11-20 10:47:33 +0530

    done

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# d_
+# Rock Paper Scissor
+
+## A rock-paper-scissor game built with tkinter in python.
+
+## Points to take care(since container opens a gui):
+
+1. Install an X server on your host machine (if it's not already installed). On Linux, you can use X.Org, which is the most common X server implementation. On Windows or macOS, you can use Xming or XQuartz, respectively.
+
+2. Open a terminal on your host machine and run the following command to allow connections from any IP address:
+   ```bash
+   xhost +
+   ```
+
+## **Docker-Compse Workflow**
+
+```bash
+1. docker-compose up // to build and start the containers
+2. docker-compose stop // to stop the containers
+3. docker-compose start // to restart the containers
+4. docker-compose down // to stop and remove the containers
+5. docker-compose up --build // to force build and start the containers
+```
+
+## **Docker setup for production and development**
+
+1. docker-compose -f docker-compose.dev.yml up
+2. docker-compose -f docker-compose.prod.yml up
--- game.py
+++ game.py
@@ -1,11 +1,11 @@
-from tkinter import *
-from PIL import Image,ImageTk
+# from tkinter import *
+import tkinter
+from PIL import Image, ImageTk
 import random
 
-root = Tk()
+root = tkinter.Tk()
 root.title("Rock-Paper-Scissors")
 root.configure(background="black")
-
 
 
 rock_i = ImageTk.PhotoImage(Image.open("rock_i.jpg"))
@@ -15,105 +15,119 @@ sciss_i = ImageTk.PhotoImage(Image.open("scis_i.jpg"))
 pap_ci = ImageTk.PhotoImage(Image.open("pap_i.jpg"))
 sciss_ci = ImageTk.PhotoImage(Image.open("scis_i.jpg"))
 
-user_label = Label(root,image=rock_i,bg="black")
-user_label.grid(row=2,column=0)
-comp_label = Label(root,image=rock_i,bg="black")
-comp_label.grid(row=2,column=4)
+user_label = tkinter.Label(root, image=rock_i, bg="black")
+user_label.grid(row=2, column=0)
+comp_label = tkinter.Label(root, image=rock_i, bg="black")
+comp_label.grid(row=2, column=4)
 
-uscore = Label(root,text=0,font=100,bg="black",fg="white")
-uscore.grid(row=2,column=1)
-cscore = Label(root,text=0,font=100,bg="black",fg="white")
-cscore.grid(row=2,column=3)
-count = Label(root,text=0,font=100,bg="black",fg="white")
-count.grid(row=1,column=2)
+uscore = tkinter.Label(root, text=0, font=100, bg="black", fg="white")
+uscore.grid(row=2, column=1)
+cscore = tkinter.Label(root, text=0, font=100, bg="black", fg="white")
+cscore.grid(row=2, column=3)
+count = tkinter.Label(root, text=0, font=100, bg="black", fg="white")
+count.grid(row=1, column=2)
 
-user_indica =  Label(root,font=50,text="YOU",bg="black",fg="white")
-user_indica.grid(row=0,column=1)
-comp_indica =  Label(root,font=50,text="BOT",bg="black",fg="white")
-comp_indica.grid(row=0,column=3)
-count_indica = Label(root,font=50,text="TRIES",bg="black",fg="white")
-count_indica.grid(row=0,column=2)
+user_indica = tkinter.Label(root, font=50, text="YOU", bg="black", fg="white")
+user_indica.grid(row=0, column=1)
+comp_indica = tkinter.Label(root, font=50, text="BOT", bg="black", fg="white")
+comp_indica.grid(row=0, column=3)
+count_indica = tkinter.Label(
+    root, font=50, text="TRIES", bg="black", fg="white")
+count_indica.grid(row=0, column=2)
 
-msg = Label(root,font=50,bg="black",fg="white")
-msg.grid(row=4,column=2)
+msg = tkinter.Label(root, font=50, bg="black", fg="white")
+msg.grid(row=4, column=2)
+
 
 def upmsg(i):
-	msg['text']=i
+    msg['text'] = i
+
+
 def click():
-	sc=int(count["text"])
-	sc+=1
-	count["text"]=str(sc)
+    sc = int(count["text"])
+    sc += 1
+    count["text"] = str(sc)
+
 
 def upuserscore():
-	sc=int(uscore["text"])
-	sc+=1
-	uscore["text"]=str(sc)
+    sc = int(uscore["text"])
+    sc += 1
+    uscore["text"] = str(sc)
+
+
 def upcompscore():
-	sc=int(cscore["text"])
-	sc+=1
-	cscore["text"]=str(sc)
-def wim(x,y):
-	if x==0:
-		if y==1:
-			upcompscore()
-		elif y==2:
-			upuserscore()
-	elif x==1:
-		if y==0:
-			upuserscore()
-		elif y==2:
-			upcompscore()
-	elif x==2:
-		if y==0:
-			upcompscore()
-		elif y==1:
-			upuserscore()
-	if int(uscore["text"])==int(cscore["text"]):
-		upmsg("TIE!")
-	elif int(uscore["text"])>int(cscore["text"]):
-		upmsg("WON!")
-	elif int(uscore["text"])<int(cscore["text"]):
-		upmsg("LOST!") 
-	click()
-		
-	
+    sc = int(cscore["text"])
+    sc += 1
+    cscore["text"] = str(sc)
+
+
+def wim(x, y):
+    if x == 0:
+        if y == 1:
+            upcompscore()
+        elif y == 2:
+            upuserscore()
+    elif x == 1:
+        if y == 0:
+            upuserscore()
+        elif y == 2:
+            upcompscore()
+    elif x == 2:
+        if y == 0:
+            upcompscore()
+        elif y == 1:
+            upuserscore()
+    if int(uscore["text"]) == int(cscore["text"]):
+        upmsg("TIE!")
+    elif int(uscore["text"]) > int(cscore["text"]):
+        upmsg("WON!")
+    elif int(uscore["text"]) < int(cscore["text"]):
+        upmsg("LOST!")
+    click()
+
+
 def func(x):
-	y=random.randint(0,2)
-	if y==0:
-		comp_label.configure(image=rock_ci)
-	elif y==1:
-		comp_label.configure(image=pap_ci)
-	elif y==2:
-		comp_label.configure(image=sciss_ci)
-	
-	if x==0:
-		user_label.configure(image=rock_i)
-	elif x==1:
-		user_label.configure(image=pap_i)
-	elif x==2:
-		user_label.configure(image=sciss_i)
-	wim(x,y)
+    y = random.randint(0, 2)
+    if y == 0:
+        comp_label.configure(image=rock_ci)
+    elif y == 1:
+        comp_label.configure(image=pap_ci)
+    elif y == 2:
+        comp_label.configure(image=sciss_ci)
+
+    if x == 0:
+        user_label.configure(image=rock_i)
+    elif x == 1:
+        user_label.configure(image=pap_i)
+    elif x == 2:
+        user_label.configure(image=sciss_i)
+    wim(x, y)
+
+
 def reset():
-	
-	sc=int(uscore["text"])
-	sc=0
-	uscore["text"]=str(sc)
-	sc=int(cscore["text"])
-	sc=0
-	cscore["text"]=str(sc)
-	upmsg("")
-	comp_label.configure(image=rock_ci)
-	user_label.configure(image=rock_i)
-	sc=int(count["text"])
-	sc=0
-	count["text"]=str(sc)
-		
+
+    sc = int(uscore["text"])
+    sc = 0
+    uscore["text"] = str(sc)
+    sc = int(cscore["text"])
+    sc = 0
+    cscore["text"] = str(sc)
+    upmsg("")
+    comp_label.configure(image=rock_ci)
+    user_label.configure(image=rock_i)
+    sc = int(count["text"])
+    sc = 0
+    count["text"] = str(sc)
 
 
-rock = Button(root,width=20,height=2,text="ROCK",bg="black",fg="white",command=lambda:func(0)).grid(row=3,column=1)
-paper = Button(root,width=20,height=2,text="PAPER",bg="black",fg="white",command=lambda:func(1)).grid(row=3,column=2)
-sciss = Button(root,width=20,height=2,text="SCISSORS",bg="black",fg="white",command=lambda:func(2)).grid(row=3,column=3)
-re = Button(root,width=2,height=2,text="re",bg="black",fg="white",command=lambda:reset()).grid(row=3,column=4)
+rock = tkinter.Button(root, width=20, height=2, text="ROCK", bg="black",
+                      fg="white", command=lambda: func(0)).grid(row=3, column=1)
+paper = tkinter.Button(root, width=20, height=2, text="PAPER", bg="black",
+                       fg="white", command=lambda: func(1)).grid(row=3, column=2)
+sciss = tkinter.Button(root, width=20, height=2, text="SCISSORS",
+                       bg="black", fg="white", command=lambda: func(2)).grid(row=3, column=3)
+re = tkinter.Button(root, width=2, height=2, text="re", bg="black",
+                    fg="white", command=lambda: reset()).grid(row=3, column=4)
 
 
 root.mainloop()
